f9721bb9caf676cde498f1ab0757fb1e039e0061
Ethan Zitting <ethanzitting@gmail.com>
2020-11-05 18:25:44 -0700

    Added tie detection, made plays permenant until end of game, added reset button.

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
-
-
 // Game board module.
 var gameBoard = (function() {
+	// 'use strict' for bugproofing.
 	'use strict';
+
+	// board for tracking game board inside program.
 	const board = ["", "", "", "", "", "", "", "", ""];
+
+	// cells for tracking game board in DOM
 	const cells = Array.from(document.querySelectorAll(".cell"));
+
+	// variables for use throughout program
 	let winner = false;
 	let player1Score = 0;
 	let player2Score = 0;
+	let turnCounter = 0;
+	const turnDisplay = document.querySelector("#turnDisplay");
+	const outcome = document.querySelector("#gameOutcome");
 
-	// Establish an event listener on the cells that will set the clicked
-	// cell to X or O alternatingly.
 
+	// Turns the cell divs into buttons and provides most of the game functionality
+	cells.forEach(cell => {
+		cell.addEventListener('click', () => {
+			// Checks if turnCounter is even to keep track of who's turn it is.
+			// Sets cell and turnDisplay in DOM
+			if (cell.textContent == "X" || cell.textContent == "O") {
+				return;
+			}
 
-	function setCell(cellID, value) {
-		cells.forEach(cell => {
-			cell.addEventListener('click', () => {
-				cell.textContent = "X"
-			});
+			if (winner !== false) {
+				return;
+			}
+			
+			if (turnCounter % 2 === 0) {
+				cell.textContent = "X";
+				turnDisplay.textContent = "Player Two's Turn";
+			} else {
+				cell.textContent = "O";
+				turnDisplay.textContent = "Player One's Turn";
+			}
+
+			turnCounter++;
+
+			// Sets cell in program
+			setCell(cell.dataset.index, cell.textContent);
 		});
+	});
 
+
+	// Sets cell in program and runs checkForWin after every move.
+	function setCell(cellID, value) {
 		board[cellID] = value;
 		checkForWin();
 	}
 
 
-	setCell(0, "X");
-	setCell(1, "X");
-	setCell(2, "O");
-	
-
+	// Checks for all possible win combinations. If found, runs declareWinner.
 	function checkForWin() {
 		function compareCells(cell1, cell2, cell3) {
 			if (cell1 == "X" || cell1 == "O") {
 				if (cell1 == cell2 && cell2 == cell3) {
-					console.log("declareWinner activated");
+					turnCounter = 0;
+					turnDisplay.textContent = "Game Over";
 					declareWinner(cell1);
-					return true;
+					return;
 				}
 			}
 		}
@@ -48,30 +74,45 @@ var gameBoard = (function() {
 		compareCells(board[2], board[5], board[8]);
 		compareCells(board[0], board[4], board[8]);
 		compareCells(board[2], board[4], board[6]);
+
+		if (turnCounter == 9 && turnDisplay.textContent !== "Game Over") {
+			turnDisplay.textContent = "Game Over";
+			outcome.textContent = "It's a tie!";
+			winner = "tie";
+			turnCounter = 0;
+		}
 	}
 
 
+	// Runs after win condition found. Provides user with flair and spectacle
+	// to signify end of game. Also resets board in DOM and program.
 	function declareWinner(roundWinner) {
 		const xScore = document.querySelector("#xScore");
 		const oScore = document.querySelector("#oScore");
-		const outcome = document.querySelector("#gameOutcome");
+
 		// Write to DOM winner Wins!
 		if (roundWinner == "X") {
 			winner = "Player 1";
 			outcome.textContent = `Player 1 wins!`;
 			player1Score++;
+			xScore.textContent = `Player One Score: ${player1Score}`;
+
 		} else if (roundWinner == "O") {
-			winner = "Player 0";
+			winner = "Player 2";
 			outcome.textContent = `Player 2 wins!`;
 			player2Score++;
-		} else {
-			winner = "Tie"
-			outcome.textContent = "It was a tie!";
+			oScore.textContent = `Player Two Score: ${player2Score}`;
 		}
+	}
+
+
+	const resetBtn = document.querySelector("#resetBtn");
+	resetBtn.addEventListener('click', () => {
 		// Clear X's and O's from Board array and DOM
 		cells.forEach(cell => cell.textContent = "");
 		for (let i in board) board[i] = "";
-	}
+		winner = false;
+	});
 
 
 	return {
@@ -81,29 +122,3 @@ var gameBoard = (function() {
 		checkForWin: checkForWin
 	}
 })();
-
-
-/*
-  //DOM Manipulation with a Module
-  const Formatter = (function(doc) {
-	const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-  
-	const makeUppercase = (text) => {
-	  log("Making uppercase");
-	  return text.toUpperCase();
-	};
-  
-	const writeToDOM = (selector, message) => {
-	  if (!!doc && "querySelector" in doc) {
-		doc.querySelector(selector).innerHTML = message;
-	  }
-	}
-  
-	return {
-	  makeUppercase,
-	  writeToDOM,
-	}
-  })(document);
-  
-  Formatter.writeToDOM("#target", "Hi there");
-  */
